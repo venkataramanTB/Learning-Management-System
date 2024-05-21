468d8d841eebbc802f472b464c4de1e1dbde6d6d
VenkataramanTB <98270783+venkataramanTB@users.noreply.github.com>
2024-05-21 19:05:23 +0530

    Update Profile.jsx

--- lms/src/Pages/Profile.jsx
+++ lms/src/Pages/Profile.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Profile.css';
-
 const Profile = () => {
     const cachedUser = sessionStorage.getItem('loggedInUser');
     const user = cachedUser ? JSON.parse(cachedUser) : null;
